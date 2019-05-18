7043d79ff694fa3da997feffea24cd1388807f27
Kotaro7750 <7750koutarou@gmail.com>
2019-05-18 23:47:02 +0900

    make mock-up of rireki list page

--- client/tcx/src/views/RirekiList.vue
+++ client/tcx/src/views/RirekiList.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="rirekilist">
     <h2>履歴リスト</h2>
+    <ul>
+        <li v-for="rireki in rirekiList" v-bind:key="rireki.id">
+            {{ rireki.id }}
+            {{ rireki.syubetsu }}
+            {{ rireki.about }}
+            {{ rireki.startTime }}
+            {{ rireki.endTime }}
+        </li>
+    </ul>
+    <button @click="fetchRireki">click</button>
   </div>
 </template>
 
@@ -11,22 +21,34 @@ import {Component, Vue} from "vue-property-decorator";
 })
 
 export default class RirekiList extends Vue{
-    rirekiList!: {
+    rirekiList: {
         id: number,
         joid:number,
         syubetsu:number,
+        about:string,
         startTime:string,
         endTime:string,
-    }[];
+    }[] = [];
+
 
     created () {
+        this.fetchRireki();
+    }
+
+    fetchRireki():void{
         fetch("http://localhost:8888/rireki/5/63",{
             method: 'GET'
         }).then(response => {
             return response.json();
         }).then(json =>{
-            console.log(json);
+            this.loadJSONToRirekiList(json);
         })
     }
+
+    loadJSONToRirekiList(json :any):void{
+        this.rirekiList = json.result;
+        console.log(this.rirekiList.length);
+    }
+
 }
 </script>
--- server/model/rireki.go
+++ server/model/rireki.go
@@ -10,12 +10,12 @@ import (
 
 //Rireki is a structure of each rireki
 type Rireki struct {
-	ID int
-	Joid      int
-	Syubetsu  int `json:"syubetsu" binding:"required"`
-	About     string `json:"about" binding:"required"`
-	StartTime time.Time `json:"start_time" binding:"required"`
-	EndTime   time.Time `json:"end_time" binding:"required"`
+	ID        int       `json:"id"`
+	Joid      int       `json:"joid"`
+	Syubetsu  int       `json:"syubetsu" binding:"required"`
+	About     string    `json:"about" binding:"required"`
+	StartTime time.Time `json:"startTime" binding:"required"`
+	EndTime   time.Time `json:"endTime" binding:"required"`
 }
 
 //PersonalRirekiAll is a function to select all data of joid from table rireki
@@ -33,7 +33,7 @@ func PersonalRirekiAll(db *sql.DB, joid int, month int) ([]*Rireki, error) {
 
 	for rows.Next() {
 		var rireki Rireki
-		if err := rows.Scan(&rireki.ID,&rireki.Joid, &rireki.Syubetsu, &rireki.About, &rireki.StartTime, &rireki.EndTime); err != nil {
+		if err := rows.Scan(&rireki.ID, &rireki.Joid, &rireki.Syubetsu, &rireki.About, &rireki.StartTime, &rireki.EndTime); err != nil {
 			return nil, err
 		}
 		rirekiList = append(rirekiList, &rireki)
@@ -48,13 +48,13 @@ func PersonalRirekiAll(db *sql.DB, joid int, month int) ([]*Rireki, error) {
 
 //RirekiAdd is a function to insert a rireki to table rireki of month
 //table should decided by client ,but month is hard coded now
-func RirekiAdd(db *sql.DB,rireki Rireki,month int)(*Rireki,error)  {
+func RirekiAdd(db *sql.DB, rireki Rireki, month int) (*Rireki, error) {
 	tableName := strconv.Itoa(month) + "_rireki"
 
 	formatedStartTime := rireki.StartTime.Format("2006-01-02 15:04:05")
 	formatedEndTime := rireki.EndTime.Format("2006-01-02 15:04:05")
-	
-	_, err := db.Query(fmt.Sprintf("INSERT INTO `%s` (joid,syubetsu,about,start_time,end_time) VALUES(%d,%d,\"%s\",cast('%s' as DATETIME),cast('%s' as DATETIME))", tableName, rireki.Joid,rireki.Syubetsu,rireki.About,formatedStartTime,formatedEndTime))
+
+	_, err := db.Query(fmt.Sprintf("INSERT INTO `%s` (joid,syubetsu,about,start_time,end_time) VALUES(%d,%d,\"%s\",cast('%s' as DATETIME),cast('%s' as DATETIME))", tableName, rireki.Joid, rireki.Syubetsu, rireki.About, formatedStartTime, formatedEndTime))
 
 	if err != nil {
 		return nil, err
@@ -64,63 +64,62 @@ func RirekiAdd(db *sql.DB,rireki Rireki,month int)(*Rireki,error)  {
 }
 
 //IsContradicted is a function to decide if given rireki's is contradicted
-func IsContradicted(db *sql.DB,rireki Rireki,month int) ([]*Rireki,error) {
+func IsContradicted(db *sql.DB, rireki Rireki, month int) ([]*Rireki, error) {
 	tableName := strconv.Itoa(month) + "_rireki"
 
 	formatedStartTime := rireki.StartTime.Format("2006-01-02 15:04:05")
 	formatedEndTime := rireki.EndTime.Format("2006-01-02 15:04:05")
 
-	rows,err := db.Query(fmt.Sprintf("SELECT id,joid,syubetsu,about,start_time,end_time FROM `%s` WHERE (joid = '%d') AND ((`start_time` BETWEEN cast('%s' as DATETIME) AND cast('%s' as DATETIME)) OR (`end_time` BETWEEN cast('%s' as DATETIME) AND cast('%s' as DATETIME)))",tableName,rireki.Joid,formatedStartTime,formatedEndTime,formatedStartTime,formatedEndTime))
-	if err !=nil {
+	rows, err := db.Query(fmt.Sprintf("SELECT id,joid,syubetsu,about,start_time,end_time FROM `%s` WHERE (joid = '%d') AND ((`start_time` BETWEEN cast('%s' as DATETIME) AND cast('%s' as DATETIME)) OR (`end_time` BETWEEN cast('%s' as DATETIME) AND cast('%s' as DATETIME)))", tableName, rireki.Joid, formatedStartTime, formatedEndTime, formatedStartTime, formatedEndTime))
+	if err != nil {
 		fmt.Print("1")
-		return nil,err
+		return nil, err
 	}
 
 	var contradictedList []*Rireki
 
-	for rows.Next(){
+	for rows.Next() {
 		var contradictedRireki Rireki
-		if err := rows.Scan(&contradictedRireki.ID,&contradictedRireki.Joid, &contradictedRireki.Syubetsu, &contradictedRireki.About, &contradictedRireki.StartTime, &contradictedRireki.EndTime); err != nil {
+		if err := rows.Scan(&contradictedRireki.ID, &contradictedRireki.Joid, &contradictedRireki.Syubetsu, &contradictedRireki.About, &contradictedRireki.StartTime, &contradictedRireki.EndTime); err != nil {
 			fmt.Print("2")
-			return nil,err
+			return nil, err
 		}
 		contradictedList = append(contradictedList, &contradictedRireki)
 	}
 
-	if len(contradictedList) !=0 {
-		return contradictedList,nil
+	if len(contradictedList) != 0 {
+		return contradictedList, nil
 	}
 
-	return nil,nil
+	return nil, nil
 }
 
 //RirekiDelete is a function to delete rireki
-func RirekiDelete(db *sql.DB,month int,joid int,id int) error {
+func RirekiDelete(db *sql.DB, month int, joid int, id int) error {
 	tableName := strconv.Itoa(month) + "_rireki"
 
-	res,err := db.Exec(fmt.Sprintf("DELETE FROM `%s` WHERE (id = '%d' AND joid = '%d')",tableName,id,joid))
+	res, err := db.Exec(fmt.Sprintf("DELETE FROM `%s` WHERE (id = '%d' AND joid = '%d')", tableName, id, joid))
 
 	if err != nil {
 		return err
 	}
 
-	if affectedRow,_ := res.RowsAffected(); affectedRow == 0{
+	if affectedRow, _ := res.RowsAffected(); affectedRow == 0 {
 		return errors.New("no rows affected")
 	}
-	return nil	
+	return nil
 }
 
 //RirekiUpdate is a function to update rireki
-func RirekiUpdate(db *sql.DB,month int,joid int,id int,rireki Rireki) error {
+func RirekiUpdate(db *sql.DB, month int, joid int, id int, rireki Rireki) error {
 	tableName := strconv.Itoa(month) + "_rireki"
 
 	formatedStartTime := rireki.StartTime.Format("2006-01-02 15:04:05")
 	formatedEndTime := rireki.EndTime.Format("2006-01-02 15:04:05")
 
-	_,err := db.Exec(fmt.Sprintf("UPDATE `%s` SET joid=%d,syubetsu=%d,about='%s',start_time=cast('%s' as DATETIME),end_time=cast('%s' as DATETIME) WHERE joid = '%d' AND id = '%d'",tableName,rireki.Joid,rireki.Syubetsu,rireki.About,formatedStartTime,formatedEndTime,joid,id))
+	_, err := db.Exec(fmt.Sprintf("UPDATE `%s` SET joid=%d,syubetsu=%d,about='%s',start_time=cast('%s' as DATETIME),end_time=cast('%s' as DATETIME) WHERE joid = '%d' AND id = '%d'", tableName, rireki.Joid, rireki.Syubetsu, rireki.About, formatedStartTime, formatedEndTime, joid, id))
 
-
-	if  err !=nil {
+	if err != nil {
 		fmt.Print(err)
 		return err
 	}
