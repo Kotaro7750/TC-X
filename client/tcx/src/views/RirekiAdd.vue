71972a38d18cbb45bf869c224426e41b1b42867b
Kotaro7750 <7750koutarou@gmail.com>
2019-05-19 09:27:04 +0900

    modify mock up of adding rireki

--- client/tcx/src/views/RirekiAdd.vue
+++ client/tcx/src/views/RirekiAdd.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="rirekiadd">
     <h2>履歴追加</h2>
-    <input type="number" v-bind:value="rireki.joid">
-    <input type="number" v-bind:value="rireki.syubetsu">
-    <input type="text" v-bind:value="rireki.about">
-    <button @click="addRireki">click</button>
+    <input type="number" v-model="rireki.joid">
+    <input type="number" v-model="rireki.syubetsu">
+    <input type="text" v-model="rireki.about">
+    <input type="time" v-model="rireki.startTime">
+    <input type="time" v-model="rireki.endTime">
+    {{rireki}}
+    <button @click="log">button</button>
   </div>
 </template>
 
@@ -15,23 +18,14 @@ import {Component, Vue} from "vue-property-decorator";
 })
 
 export default class RirekiAdd extends Vue{
-    rireki!: {
+    rireki:{
         joid:number,
         syubetsu:number,
         about:string,
         startTime:string,
         endTime:string,
-    };
+    } ={joid:63,syubetsu:3,about:'',startTime:'',endTime:''};
 
-    created(){
-      this.rireki = {
-        joid:63,
-        syubetsu:12,
-        about:"abababa",
-        startTime:"2019-05-01T12:58:00Z",
-        endTime:"2019-05-01T12:56:00Z"
-      }
-    }
 
     addRireki():void{
       var url = "http://localhost:8888/rireki/" + "5";
