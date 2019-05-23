fa484adfc49ddc180a24d12b90cc76541633921e
Kotaro <7750koutarou@gmail.com>
2019-05-23 21:02:16 +0900

    make vuex store

--- client/tcx/src/main.ts
+++ client/tcx/src/main.ts
@@ -1,11 +1,13 @@
 import Vue from 'vue';
+import Vuex from 'vuex';
 import App from './App.vue';
 import router from './router';
 
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
+Vue.use(Vuex);
 
 Vue.config.productionTip = false;
 
@@ -13,3 +15,29 @@ new Vue({
   router,
   render: (h) => h(App),
 }).$mount('#app');
+
+const store = new Vuex.Store({
+  state: {
+    // when singnin, this represent joid of that user. if not, -1
+    userInfo: {
+      signInJoid: -1,
+      apiToken: '',
+    },
+  },
+
+  getters: {
+    joid: (state) => state.userInfo.signInJoid,
+    token: (state) => state.userInfo.apiToken,
+  },
+
+  mutations: {
+    signIn(state: any , payload: {joid: number, token: string}) {
+      state.userInfo.signInJoid = payload.joid;
+      state.userInfo.apiToken = payload.token;
+    },
+    signOut(state) {
+      state.userInfo.signInJoid = -1;
+      state.userInfo.apiToken = '';
+    },
+  },
+});
